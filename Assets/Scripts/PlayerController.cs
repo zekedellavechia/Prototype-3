e33a0d753db6acb82610c3bf4c0d4c16b2059b71
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2020-08-25 07:35:19 -0300

    Prototype 3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    [Tooltip("Fuerza del salto")] [Range(0, 50)] public float jumpForce = 5.0f;
+    [Tooltip("Control de gravedad")] [Range(0, 50)] public float gravityModifier = 5;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obtener el rigidbody 
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+       
+    }
+
+  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    public float timeStart = 2f;
+    public float timeRepeat = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", timeStart, timeRepeat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+    }
+}
